61589cbcdf6f476050c5fc132cc395c24b34df67
libin <libin2@ctsi.com.cn>
2018-04-13 18:10:52 +0800

    first commit

---
+++ src/main/java/com/ctsi/jsondemo/App.java
@@ -0,0 +1,19 @@
+package com.ctsi.jsondemo;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World!");
+		
+		
+	}
+	
+}
