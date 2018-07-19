1b93377de380f12780d1487ac5017a172cfbf39a
libin <libin2@ctsi.com.cn>
2018-07-19 11:04:30 +0800

    save

--- src/main/java/com/ctsi/jsondemo/App.java
+++ src/main/java/com/ctsi/jsondemo/App.java
@@ -1,6 +1,9 @@
 package com.ctsi.jsondemo;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 
 
 /**
@@ -9,10 +12,33 @@ import com.fasterxml.jackson.databind.ObjectMapper;
  */
 public class App {
 	
-	public static void main(String[] args) {
+	
+	public static void main(String[] args) throws IOException {
 		
 		System.out.println("Hello World!");
 		
+		Infos info = new Infos();
+		info.setDesc("根目录只有一个属性就是我");
+		
+		Job job2 = new Job();
+		job2.setName("Job-2");
+		job2.setDescription("我是Job2");
+		
+		Job job3 = new Job();
+		job3.setName("Job-3");
+		job3.setDescription("我是一个空Job");
+		
+		List<Job> jobs = new ArrayList<>();
+		jobs.add(job2);
+		jobs.add(job3);
+		
+		info.setJobs(jobs);
+		
+		String result = JacksonUtil.bean2Json(info);
+		System.out.println(result);
+		
+		Infos infos = JacksonUtil.json2Bean(result, Infos.class);
+		System.out.println(infos);
 		
 	}
 	
---
+++ src/main/java/com/ctsi/jsondemo/Infos.java
@@ -0,0 +1,26 @@
+package com.ctsi.jsondemo;
+
+import java.util.List;
+
+public class Infos {
+
+	private String desc;
+	private List<Job> jobs;
+
+	public String getDesc() {
+		return desc;
+	}
+
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+
+	public List<Job> getJobs() {
+		return jobs;
+	}
+
+	public void setJobs(List<Job> jobs) {
+		this.jobs = jobs;
+	}
+
+}
