912da857a1f6eebf099c2ec9efe4e228d71370c6
libin <libin2@ctsi.com.cn>
2018-07-27 15:22:46 +0800

    save

---
+++ src/main/java/com/ctsi/jsondemo/Job.java
@@ -0,0 +1,70 @@
+package com.ctsi.jsondemo;
+
+public class Job {
+
+	private String name;
+	private String description;
+	private String displayName;
+	private String displayNameOrNull;
+	private String fullDisplayName;
+	private String fullName;
+	private String url;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getDisplayName() {
+		return displayName;
+	}
+
+	public void setDisplayName(String displayName) {
+		this.displayName = displayName;
+	}
+
+	public String getDisplayNameOrNull() {
+		return displayNameOrNull;
+	}
+
+	public void setDisplayNameOrNull(String displayNameOrNull) {
+		this.displayNameOrNull = displayNameOrNull;
+	}
+
+	public String getFullDisplayName() {
+		return fullDisplayName;
+	}
+
+	public void setFullDisplayName(String fullDisplayName) {
+		this.fullDisplayName = fullDisplayName;
+	}
+
+	public String getFullName() {
+		return fullName;
+	}
+
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+}
+
